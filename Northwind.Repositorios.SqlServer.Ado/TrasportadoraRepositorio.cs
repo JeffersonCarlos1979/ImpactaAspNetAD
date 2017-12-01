cc5f77873f125ce2bfe94dc912ce69bf2b76fb11
JeffersonCarlos1979 <jefferson.carlos@weightech.com.br>
2017-11-30 22:32:48 -0200

    Aula 4

--- Northwind.Repositorios.SqlServer.Ado/TrasportadoraRepositorio.cs
+++ Northwind.Repositorios.SqlServer.Ado/TrasportadoraRepositorio.cs
@@ -10,13 +10,13 @@ using System.Threading.Tasks;
 
 namespace Northwind.Repositorios.SqlServer.Ado
 {
-    public class TrasportadoraRepositorio : RepositorioBase
+    public class TrasportadoraRepositorio : RepositorioListBase
     {
         private String _stringConexao =
             ConfigurationManager
                 .ConnectionStrings["NorthwindConnectionString"]
                 .ConnectionString;
-        
+
         public List<Transportadora> Selecionar()
         {
             var transportadoras = new List<Transportadora>();
@@ -43,6 +43,100 @@ namespace Northwind.Repositorios.SqlServer.Ado
             }
 
             return transportadoras;
+        }
+
+        public Transportadora Selecionar(int id)
+        {
+            Transportadora transportadora = null;
+
+            using (var conexao = new SqlConnection(_stringConexao))
+            {
+                conexao.Open();
+
+                using (var comando = new SqlCommand("TransportadoraSelecionar", conexao))
+                {
+                    comando.CommandType = CommandType.StoredProcedure;
+                    comando.Parameters.Add(new SqlParameter("@ShipperID", id));
+
+                    using (var registro = comando.ExecuteReader())
+                    {
+                        if (registro.Read())
+                        {
+                            transportadora = Mapear(registro);
+                        }
+                    }
+                }
+            }
+            return transportadora;
+        }
+
+        public void Inserir(Transportadora transportadora)
+        {
+
+            using (var conexao = new SqlConnection(_stringConexao))
+            {
+                conexao.Open();
+
+                using (var comando = new SqlCommand("TransportadoraInserir", conexao))
+                {
+                    comando.CommandType = CommandType.StoredProcedure;
+                    comando.Parameters.AddRange(Mapear(transportadora));
+
+                    transportadora.Id = Convert.ToInt32(comando.ExecuteScalar());
+                }
+            }
+        }
+
+        public void Atualizar(Transportadora transportadora)
+        {
+
+            //using (var conexao = new SqlConnection(_stringConexao))
+            //{
+            //    conexao.Open();
+
+            //    using (var comando = new SqlCommand("TransportadoraAtualizar", conexao))
+            //    {
+            //        comando.CommandType = CommandType.StoredProcedure;
+            //        comando.Parameters.AddRange(Mapear(transportadora));
+
+            //        comando.ExecuteNonQuery();
+            //    }
+            //}
+            ExecuteNomQuery("TransportadoraAtualizar", Mapear(transportadora));
+        }
+
+        public void Excluir(Int32 id)
+        {
+
+            //using (var conexao = new SqlConnection(_stringConexao))
+            //{
+            //    conexao.Open();
+
+            //    using (var comando = new SqlCommand("TransportadoraExcluir", conexao))
+            //    {
+            //        comando.CommandType = CommandType.StoredProcedure;
+            //        //comando.Parameters.Add(new SqlParameter("@ShipperID", id));
+            //        comando.Parameters.AddWithValue("@ShipperID", id);
+            //        comando.ExecuteNonQuery();
+            //    }
+            //}
+            ExecuteNomQuery("TransportadoraExcluir", new SqlParameter("@ShipperID", id));
+        }
+
+        private SqlParameter[] Mapear(Transportadora transportadora)
+        {
+            //SqlParameter[] parametros = new List<SqlParameter>();
+            var parametros = new List<SqlParameter>() {
+                new SqlParameter("@companyName", transportadora.Nome),
+                new SqlParameter("@phone",transportadora.Telefone)
+            };
+
+            if (transportadora.Id>0)
+            {
+                parametros.Add(new SqlParameter("@ShipperID", transportadora.Id));
+            }
+
+            return parametros.ToArray();
         }
 
         private Transportadora Mapear(SqlDataReader registro)
--- Northwind.Repositorios.SqlServer.AdoTests/TrasportadoraRepositorioTests.cs
+++ Northwind.Repositorios.SqlServer.AdoTests/TrasportadoraRepositorioTests.cs
@@ -1,22 +1,20 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Northwind.Repositorios.SqlServer.Ado;
+using Northwind.Dominio;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Northwind.Repositorios.SqlServer.Ado.Tests
 {
     [TestClass()]
     public class TrasportadoraRepositorioTests
     {
+        TrasportadoraRepositorio _rep = new TrasportadoraRepositorio();
+
         [TestMethod()]
         public void SelecionarTest()
         {
-            var rep = new TrasportadoraRepositorio();
 
-            var transportadoras = rep.Selecionar();
+            var transportadoras = _rep.Selecionar();
 
             Assert.AreNotEqual(0, transportadoras.Count);
 
@@ -24,6 +22,46 @@ namespace Northwind.Repositorios.SqlServer.Ado.Tests
             {
                 Console.WriteLine($"{transportadora.Id} - {transportadora.Nome}");
             }
+
+
+        }
+
+        [TestMethod()]
+        public void SelecionarPorIdTest()
+        {
+            var transportadora = _rep.Selecionar(2);
+            Assert.IsNotNull(transportadora);
+
+            transportadora = _rep.Selecionar(0);
+            Assert.IsNull(transportadora);
+
+
+        }
+
+        [TestMethod()]
+        public void CudTest()
+        {
+            //Create
+            var transportadora = new Transportadora();
+            transportadora.Nome = $"Trans_{DateTime.Now.ToString("HHmmss")}";
+            transportadora.Telefone = $"Trans_{DateTime.Now.ToString("HHmmss")}";
+            _rep.Inserir(transportadora);
+
+            //Update
+            String novoNome = $"Trans{DateTime.Now.ToString("HHmmss")}_Editado";
+            transportadora.Nome = novoNome;
+            _rep.Atualizar(transportadora);
+
+            transportadora = _rep.Selecionar(transportadora.Id);
+
+            Assert.IsTrue(transportadora.Nome==novoNome);
+
+            _rep.Excluir(transportadora.Id);
+
+            transportadora = _rep.Selecionar(transportadora.Id);
+
+            Assert.IsNull(transportadora);
+
 
         }
     }
