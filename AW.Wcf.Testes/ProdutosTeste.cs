d319a70fda37f5367904b98862b1314c7ff7cdc3
JeffersonCarlos1979 <jefferson.carlos@weightech.com.br>
2017-12-08 22:34:26 -0200

    Aula 10

---
+++ AW.Wcf.Testes/ProdutosTeste.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AW.Wcf.Testes.ProdutosServiceReference;
+
+namespace AW.Wcf.Testes
+{
+    [TestClass]
+    public class ProdutosTeste
+    {
+        [TestMethod]
+        public void SelecionarTest()
+        {
+
+            using (var produtosClient = new ProdutosClient())
+            {
+                var produto = produtosClient.Selecionar(316);
+
+                Assert.AreEqual(produto.Name,"Blade");
+            }
+        }
+
+        [TestMethod]
+        public void SelecionarPorNomeTest()
+        {
+
+            using (var produtosClient = new ProdutosClient())
+            {
+                var produtos = produtosClient.SelecionarPorNome("mountain");
+
+                Assert.AreEqual(produtos.Length,94);
+            }
+        }
+
+
+    }
+}
